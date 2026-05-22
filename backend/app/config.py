a04a7f6051895baf9546b003c2ae207d7dbc3371
GabrielAlves <gabriel0x3d@gmail.com>
2026-05-21 23:23:55 -0300

    [refactor] comentando temporariamente as variáveis da AWS

--- backend/app/config.py
+++ backend/app/config.py
@@ -14,7 +14,7 @@ class Config:
 
     STORAGE_MODE = os.getenv("STORAGE_MODE", "local")
 
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_BUCKET = os.getenv("AWS_BUCKET")
-    AWS_REGION = os.getenv("AWS_REGION")
+    # AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    # AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    # AWS_BUCKET = os.getenv("AWS_BUCKET")
+    # AWS_REGION = os.getenv("AWS_REGION")
