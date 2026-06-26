605b838b7935a73fc5fb68b41ecbec2f7d898d2c
GabrielAlves <gabriel0x3d@gmail.com>
2026-06-26 20:32:18 -0300

    [refactor] acessa o front

--- script_deploy.sh
+++ script_deploy.sh
@@ -10,7 +10,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 echo "Configurando banco de dados..."
-sudo mysql -u root -p < db_setup.sql
+echo $SENHA | sudo -S mysql -u root -p < db_setup.sql
 
 echo "Iniciando backend..."
 
@@ -20,7 +20,7 @@ pip install -r requirements.txt
 cd app
 
 cat > .env <<EOF
-API_KEY=my-secret-key
+API_KEY=test_key
 STORAGE_MODE=local # "local" or "s3"
 SQLALCHEMY_DATABASE_URI=mysql+mysqlconnector://admin:admin@localhost:3306/db
 EOF
@@ -49,7 +49,8 @@ cd ..
 
 echo "Aguardando aplicação..."
 
-until curl -sf http://localhost:8000/health > /dev/null
+# checa se o frontend está disponível. O frontend depende do back e do bd no docker-compose
+until curl -sf http://localhost:5001/ > /dev/null
 do
     sleep 1
 done
